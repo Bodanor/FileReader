bec9340425f2dda493315fa78b36d6d446a55570
Bodanor <papadopoulos.chris@icloud.com>
2019-05-20 22:56:29 +0200

    Reorganised code plus adding loops

--- FileReader/main.cpp
+++ FileReader/main.cpp
@@ -10,54 +10,76 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <boost/filesystem.hpp>
 using namespace std;
 int main(int argc, const char * argv[]) {
     std::string database_data;
     int database_count = 0;
-    std::string database_path;
-    std::string desiredDBData;
-    std::cout << "Please enter the path to desired the database -->";
-    std::cin >> database_path;
-    ifstream database (database_path);
-    
-    if (database)
-    {
-        cout << "\nDatabase Found !\n"<<endl;
-    }
-    else {
-        cout << "\nAn error occured while trying to open the database. Maybe you have entered a none valid path. Please check and try again !\n"<<endl;
-        exit(1);
-    }
-    cout << "Loading Database please be patient ...\n\n";
-    vector<string> DBData;
-    while (getline(database,database_data))
-    {
-        database_count ++;
-        DBData.push_back(database_data);
-        
-    }
-    cout << "Database loaded succesfully !\n";
-    
-    cout << "Database lines -->" << database_count<<"\n\n"<<endl;
-    cout << "Enter the desired Data -->";
-    cin >> desiredDBData;
-    cout << "Looking for the desired Data. Please be patient ...\n"<<endl;
-    for (int x = 0; x <= database_count; x++)
-    {
-        if (desiredDBData == DBData[x])
+    int choice = 0;
+    while (choice !=1){
+        std::string database_path;
+        std::string desiredDBData;
+        std::cout << "Please enter the path where your Databse is -->";
+        std::cin >> database_path;
+        boost::filesystem::path database_1(database_path);
+        ifstream database (database_path);
+        if (database)
         {
-            cout <<"Data Founded at line "<< x+1 << " and contains data --> "<< DBData[x] <<endl;
-            
-            
-        }
-        else{
-            if (x == database_count)
+            choice = 1;
+            cout << "\nDatabase Found !\n"<<endl;
+            cout << "Loading Database please be patient ...\n\n";
+            vector<string> DBData;
+            while (getline(database,database_data))
             {
-                cout << "Nothing has been found !\n";
+                database_count ++;
+                DBData.push_back(database_data);
+                
             }
-            else
+            cout << "Database loaded succesfully !\n";
+            cout << "Database lines -->" << database_count<<"\n\n"<<endl;
+            cout << "Enter the desired Data -->";
+            cin >> desiredDBData;
+            cout << "Looking for the desired Data. Please be patient ...\n"<<endl;
+            for (int x = 0; x <= database_count; x++)
             {
-                NULL;
+                if (desiredDBData == DBData[x])
+                {
+                    cout <<"Data Founded at line "<< x+1 << " and contains data --> "<< DBData[x] <<endl;
+                    
+                    
+                }
+                else{
+                    if (x == database_count)
+                    {
+                        cout << "Nothing has been found !\n";
+                    }
+                    else
+                    {
+                        NULL;
+                    }
+                }
+            }
+        }
+        else {
+            
+            cout << "\nAn error occured while trying to open the database. Maybe you have entered a none valid path. Please check and try again !\n\nWould you like to:\n\n(1)Retry\n(2)Exit"<<endl;
+            int error = 0;
+            while (error != 1)
+            {
+                cin >> choice;
+                if (choice == 1)
+                {
+                    error = 1;
+                    choice = 0;
+                }
+                else if (choice == 2)
+                {
+                    exit(1);
+                }
+                else{
+                    cout << "Please use a correct option !";
+                    error = 0;
+                }
             }
         }
     }
